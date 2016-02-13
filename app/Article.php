4a2a6c9c88da4f3faae4c2ad1067596d6f08aefe
nath <nath@noizers.com>
2016-02-13 12:25:28 +0100

    Add php doc

--- app/Article.php
+++ app/Article.php
@@ -7,17 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Article
  *
- * @property-read \App\Author $author
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $comments
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Like[] $likes
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[] $reports
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Tag[] $tags
  * @property integer $id
+ * @property integer $node_id
  * @property integer $author_id
  * @property string $title
  * @property string $body
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Author $author
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $comments
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Like[] $likes
+ * @property-read \App\Node $node
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[] $reports
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Tag[] $tags
  */
 class Article extends Model
 {
--- app/Node.php
+++ app/Node.php
@@ -4,6 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Node
+ *
+ * @property integer $id
+ * @property integer $parent_id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Article[] $articles
+ * @property-read \App\Node $parent
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Node[] $children
+ */
 class Node extends Model
 {
     public function articles()
